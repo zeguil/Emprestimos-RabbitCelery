ce57685224f2f827648f9ba95e9413e40d82abb3
Jose Guilherme <zeguilhermelins@hotmail.com>
2023-06-14 08:48:59 -0400

    create proposta model

--- propostas/models.py
+++ propostas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Proposta(models.Model):
+    nome_completo = models.CharField(max_length=80)
+    cpf = models.CharField(max_length=11)
+    endereco = models.CharField(max_length=200)
+    valor_emprestimo = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=[('Aprovada', 'Aprovada'), ('Negada', 'Negada')], default='Aguardando')
+    
+    def __str__(self):
+        return self.nome_completo
