100717f388a639174e925f2077d88773b3d4024c
Jose Guilherme <zeguilhermelins@hotmail.com>
2023-06-20 12:38:23 -0400

    edit admin ui

--- backend/propostas/admin.py
+++ backend/propostas/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Proposta
 
-admin.site.register(Proposta)
+class PropostaAdmin(admin.ModelAdmin):
+    list_display = ('nome_completo', 'status') 
+    list_filter = ('status',)  
+
+admin.site.register(Proposta, PropostaAdmin)
